67229b8add650c923995d341d6a6e8e04ab0eb7a
Abdul3147 <abdallahussein228@gmail.com>
2024-07-01 11:11:14 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code in this file!
+const currentUser = "Abdul";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+
+console.log(currentUser);
+console.log(welcomeMessage);
+console.log(excitedWelcomeMessage); 
+console.log(shortWelcomeMessage);
+
+
+
+
